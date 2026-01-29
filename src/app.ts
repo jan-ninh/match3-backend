6c1bc1257f29fe9c99464c34f03b2897a226e7eb
jan-ninh <janninh@gmx.de>
2026-01-29 16:50:17 +0100

    minor updates

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-import express from "express";
-import { routes } from "./routes/index.ts";
-import { notFoundHandler } from "./middlewares/notFoundHandler.ts";
-import { errorHandler } from "./middlewares/errorHandler.ts";
+import express from 'express';
+import { routes } from './routes/index.ts';
+import { notFoundHandler } from './middlewares/notFoundHandler.ts';
+import { errorHandler } from './middlewares/errorHandler.ts';
+import cors from 'cors';
 
 export const app = express();
 
-app.set("trust proxy", 1);
+app.set('trust proxy', 1);
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+);
 
 app.use((req, _res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
