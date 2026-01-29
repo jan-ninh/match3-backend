3e21ef21709bebbbdde74ad6c83dcf1100b9df4a
Jan Ninh <janninh@gmx.de>
2026-01-29 17:54:55 +0100

    Maintenance/small updates (#1)
    * minor updates
    
    * added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,30 @@
-import express from "express";
-import { routes } from "./routes/index.ts";
-import { notFoundHandler } from "./middlewares/notFoundHandler.ts";
-import { errorHandler } from "./middlewares/errorHandler.ts";
+import express from 'express';
+import { routes } from './routes/index.ts';
+import { notFoundHandler } from './middlewares/notFoundHandler.ts';
+import { errorHandler } from './middlewares/errorHandler.ts';
+import cors from 'cors';
 
 export const app = express();
 
-app.set("trust proxy", 1);
+app.set('trust proxy', 1);
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5173', 'https://match3-frontend.onrender.com'];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow server-to-server / curl / health checks
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'));
+    },
+  }),
+);
 
 app.use((req, _res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
