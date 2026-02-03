30a70151af32b93b18584045b7ca197011922749
Elsa <teaserpluss@gmail.com>
2026-02-03 17:01:12 +0100

    Create db (#3)
    * first commit
    
    * create Database
    
    * edit env Example

---
+++ src/db/index.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+  const uri = envOrThrow('MONGO_URI');
+  try {
+    await mongoose.connect(process.env.MONGO_URI!, {
+      dbName: 'Match-3',
+    });
+    console.log('\x1b[35mMongoDB connected via Mongoose\x1b[0m');
+  } catch (error) {
+    if (process.env.NODE_ENV !== 'production') console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+}
+
+function envOrThrow(key: string) {
+  if (!process.env[key]) {
+    throw new Error(`${key} is missing in .env`);
+  }
+  return process.env[key];
+}
