892b0a862b064f541a7ad1ef1f6758f58feb997d
jan-ninh <janninh@gmx.de>
2026-02-04 09:42:07 +0100

    refactor: start server from server.ts only

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import { app } from "./app.ts";
-import { env } from "./utils/env.ts";
+import { app } from './app.ts';
+import { env } from './utils/env.ts';
+import { connectDB } from '#db';
+
+await connectDB();
 
 app.listen(env.PORT, () => {
   console.log(`[server] listening on port ${env.PORT}`);
